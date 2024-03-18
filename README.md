c29030df2aee2dc3ec40cdeed5c24e1586a20938
Colin <163931048+cbm1993@users.noreply.github.com>
2024-03-18 22:25:12 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# weather-app
+# Weather App
+
+This project was my first Progressive Web App and was created with ReactJS & CSS.
+
+# How to setup
+
+1. Extract files
+2. Open node command line and browse to directory location
+3. Install app (npm install)
+4. Run app (npm start)
+
+# Features
+
+* When temperture is over 15 degrees, it shows a "warm" image and if below 15 degrees it shows a "cold" image
+* Pulls weather data from an API
+* Provides weather type ie clouds, rain etc
+* Provides current date
+
+
+# How does it work?
+
+This app works by searching the weather of any city around the world, it provides the user with information regarding the weather type aswell.
+
+# Comments
+
+This project was alot of fun and is the first PWA I have created, I implemented several different features that I hadnt done before like changing images regarding the temperature and providing real time weather type updates. I had issues regarding the CSS and making my images load, I overcame this which helped me upgrade my knowledge of CSS by educating myself on different solutions. 
+
+# View app
+https://pwa-weatherapplication.netlify.app/
---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+
+import { fetchWeather } from "./api/fetchWeather";
+import "./App.css";
+
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+
+      setWeather(data);
+      setQuery("");
+    }
+  };
+
+  return (
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 15
+            ? "app warm"
+            : "app cold"
+          : "app"
+      }
+      
+    >
+
+
+      <input
+
+        type="text"
+        className="search"
+        placeholder="Search city... "
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+
+
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="date">{dateBuilder(new Date())}</div>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
